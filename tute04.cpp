a54e4c1934143a71cdf6258f48d9d375eccd5d76
IT21345678 <99394842+IT21345678@users.noreply.github.com>
2022-02-20 08:20:10 +0000

    Tutorial2-tute04

--- tute04.cpp
+++ tute04.cpp
@@ -11,19 +11,51 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
-#include <iostream>
+# include<iostream>
 
-long Factorial(int no);
-long nCr(int n, int r);
+long fractor(int no);
+long ncr (int n,int r,int c);
 
-int main() {
-  int n, r;
-  std::cout << "Enter a value for n ";
-  std::cin >> n;
-  std::cout << "Enter a value for r ";
-  std::cin >> r;
-  std::cout << "nCr = ";
-  std::cout << nCr(n,r);
-  std::cout << std::endl;
-  return 0;
+int main(){
+	
+	int n;
+	int r ;
+	int facn;
+	int facr;
+	int facc;
+	int total;
+	
+	std::cout <<"Enter a value for n :";
+	std ::cin>> n;
+	
+	std::cout <<"Enter a value for r :";
+	std :: cin >> r;
+	
+	facn = fractor(n);
+	facr = fractor(r);
+	facc = fractor(n-r);
+	
+	
+	total = ncr(facn,facr,facc);
+	
+	std ::cout << "nCr is " <<total ;
 }
+
+long fractor(int no){
+	int count=1;
+	
+	for(int i=1;i<=no;i++){
+		
+		count = count*i;
+		
+	}
+	
+	return count;
+}
+
+long ncr (int n,int r,int c){
+	
+	return n/(r*(c));
+	
+}
+
